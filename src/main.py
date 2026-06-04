1ee4c8756f91e9a4b7bef07db3e8ff973e272da5
Hodoorio <114991680+Hodoorio@users.noreply.github.com>
2026-06-04 22:01:43 +0900

    pytest 모두 통과 완료 06.04

--- src/main.py
+++ src/main.py
@@ -4,7 +4,9 @@
 #
 # 강의 시간에 실습한 Linux 명령어를 활용하세요.
 def get_current_path_command():
-    raise NotImplementedError
+
+    return "pwd"
+    # raise NotImplementedError
 
 
 # 문제 2.
@@ -21,7 +23,13 @@ def get_current_path_command():
 #
 # 강의 시간에 사용한 옵션을 활용하세요.
 def make_list_command(hidden):
-    raise NotImplementedError
+
+    if hidden:
+        return "ls -al"
+    else:
+        return "ls"
+
+    # raise NotImplementedError
 
 
 # 문제 3.
@@ -30,7 +38,9 @@ def make_list_command(hidden):
 #
 # path 인자는 이동할 디렉토리 경로입니다.
 def make_change_directory_command(path):
-    raise NotImplementedError
+    
+    return "cd " + path
+    # raise NotImplementedError
 
 
 # 문제 4.
@@ -41,7 +51,9 @@ def make_change_directory_command(path):
 #
 # 강의 중 확인한 U-Boot 명령어를 활용하세요.
 def make_printenv_command(env_name):
-    raise NotImplementedError
+    
+    return "printenv " + env_name
+    # raise NotImplementedError
 
 
 # 문제 5.
@@ -59,4 +71,7 @@ def make_printenv_command(env_name):
 #
 # 강의 중 실습한 bootargs 형식을 참고하세요.
 def make_bootargs_command(console, rootfs):
-    raise NotImplementedError
+    
+    return f"setenv bootargs console={console} root={rootfs} rw"
+
+    # raise NotImplementedError
